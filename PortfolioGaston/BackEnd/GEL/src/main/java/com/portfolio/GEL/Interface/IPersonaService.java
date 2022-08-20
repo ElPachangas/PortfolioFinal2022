7f1d62a074016fa79757dba41e40d8d4323f8ec3
Gaston Elias <96868564+ElPachangas@users.noreply.github.com>
2022-08-19 22:32:09 -0300

    Primera parte del Back OK

---
+++ PortfolioGaston/BackEnd/GEL/src/main/java/com/portfolio/GEL/Interface/IPersonaService.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolio.GEL.Interface;
+
+import com.portfolio.GEL.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    //Traer una lista de personas
+    public List<Persona> getPersona();
+
+    //Guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+
+    //Eliminar un objeto por id
+    public void deletePersona(Long id);
+
+    //Buscar una persona por id
+    public Persona findPersona(Long id);
+}
